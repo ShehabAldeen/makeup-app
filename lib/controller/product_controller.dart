74a74c44c8864d163f1ccc308758ae56a10849a2
shehab <salshazlly3030@gmail.com>
2022-01-30 15:13:44 +0200

    makeup with MVC and getx

---
+++ lib/controller/product_controller.dart
@@ -0,0 +1,28 @@
+//get-state
+import 'package:get/state_manager.dart';
+import 'package:make_up/model/product.dart';
+import 'package:make_up/service/service_controller.dart';
+
+
+class ProductController extends GetxController {
+  var isLoading = true.obs;
+  var productList = List<Product>().obs;
+
+  @override
+  void onInit() {
+    fetchProducts();
+    super.onInit();
+  }
+
+  void fetchProducts() async {
+    try {
+      isLoading(true);
+      var products = await RemoteServices.fetchProducts();
+      if (products != null) {
+        productList.value = products;
+      }
+    } finally {
+      isLoading(false);
+    }
+  }
+}
